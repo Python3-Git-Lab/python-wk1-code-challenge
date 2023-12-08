718be8c3c3cb20b0e834fc7ee7ec84c9529302f8
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-12-08 19:09:21 +0300

    corrected solution

--- lib/time_converter.py
+++ lib/time_converter.py
@@ -1,17 +1,10 @@
 # function to convert time to 24 hrs
 
 def convert(hour , minute , period):
-    if period == "AM" and hour == 12:
-        return "0000hrs"
-    elif (period == "PM" or period== "NOON") and hour==12:
-        return "1200noon"
-    elif period == "AM" and hour in range(0, 13):
-        time = (f"{(hour-0)}{minute}hrs")
-        return time
-    elif (period == "PM" and hour in range(0,13)):
-        times = (f"{(hour+12)}{minute}hrs")
-        return times
-    else:
-        return ("Invalid time")
-converted= convert(12 , 00 , "PM")
+    if period == "pm" and hour != 12:
+        hour += 12
+    elif period == "am" and hour == 12:
+        hour = 0
+    return f"{hour:02d}{minute:02d}"
+converted= convert(12 , 10 , "am")
 print (converted)
